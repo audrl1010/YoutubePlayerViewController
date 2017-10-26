cf018dfaf7a88b1f90b44672dfe12460fccb5f74
Myung gi son <audrl1010@naver.com>
2017-10-26 22:45:02 +0900

    Initial Commit

---
+++ YoutubeVideoPlayerViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubeVideoPlayerViewController
+//
+//  Created by myung gi son on 2017. 10. 26..
+//  Copyright © 2017년 com.smg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
